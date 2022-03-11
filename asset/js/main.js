b55f56a14f3dbaf3b6e1414a5b21fa90f6c20d6b
simoneluca00 <si.zaino@libero.it>
2022-03-11 17:59:53 +0100

    aggiunta media query

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,7 +2,7 @@
 var numeroGenerale = document.getElementById("container_number");
 
 // ciclo "for" per stampa dei numeri in console
-for( x = 1; x <= 100; x++ ) {
+for( x = 1; x <= 110; x++ ) {
         // condizione per i multipli di 3 e 5 contemporaneamente
     if ( (x % 3 == 0)  && (x % 5 == 0)) {
         numeroGenerale.innerHTML += `<p class="fizzbuzz">fizzbuzz</p>`;
