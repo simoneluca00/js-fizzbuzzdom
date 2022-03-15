09fda40d1fc33b2d268835d4710b4e8bdecdb869
simoneluca00 <si.zaino@libero.it>
2022-03-15 11:02:53 +0100

    Correzione

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,17 +1,20 @@
 // definire il contenitore
-var numeroGenerale = document.getElementById("container_number");
+let numeroGenerale = document.getElementById("container_number");
 
 // ciclo "for" per stampa dei numeri in console
-for( x = 1; x <= 110; x++ ) {
+for( let x = 1; x <= 110; x++ ) {
         // condizione per i multipli di 3 e 5 contemporaneamente
     if ( (x % 3 == 0)  && (x % 5 == 0)) {
-        numeroGenerale.innerHTML += `<p class="fizzbuzz">fizzbuzz</p>`;
+        let fizzBuzz = "FizzBuzz";
+        numeroGenerale.innerHTML += `<p class="fizzbuzz">${fizzBuzz}</p>`;
         // condizione per i multipli di 3
     }  else if ( x % 3 == 0 ) {
-        numeroGenerale.innerHTML += `<p class="fizz">fizz</p>`;
+        let fizz = "Fizz";
+        numeroGenerale.innerHTML += `<p class="fizz">${fizz}</p>`;
         // condizione per i multipli di 5
     }   else if ( x % 5 == 0 ) {
-        numeroGenerale.innerHTML += `<p class="buzz">buzz</p>`;
+        let buzz = "Buzz";
+        numeroGenerale.innerHTML += `<p class="buzz">${buzz}</p>`;
         // condizione per tutti gli altri numeri nè multipli di 3 nè di 5
     }   else {
         numeroGenerale.innerHTML += `<p class="basic">${x}</p>`;
